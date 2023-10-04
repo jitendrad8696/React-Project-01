0aec61cfb5a0a2ce12da0b49e5241f4ad10e9e77
Jitendra Dhakar <Jitendrad869601@gmail.com>
2023-10-04 19:38:27 +0530

    Github Pages Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   let [count, setCount] = useState(10);
@@ -9,27 +7,24 @@ function App() {
   const add = () => {
     if (count < 20) {
       setCount(count + 1);
-    }
-    else {
+    } else {
       alert("You can't make Count more than 20");
     }
-  }
+  };
   const remove = () => {
     if (count > 0) {
       setCount(count - 1);
-    }
-    else {
+    } else {
       alert("You can't make Count less than 0");
     }
-  }
+  };
   const reset = () => {
     setCount(10);
-  }
-
+  };
 
   return (
     <>
-      <h1>Hello This is Jitendra Dhakar</h1>
+      <h1>Hello My Name is Jitendra Dhakar</h1>
       <br />
       <h2>Count : {count}</h2>
       <br />
@@ -42,7 +37,7 @@ function App() {
       <br />
       <button onClick={remove}>Remove : {count}</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
